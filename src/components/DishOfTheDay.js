243e59d478ed9e369f1122fe6b6592358335d33a
Bhekumuzi Tshabalala <Bhekumuzitshabalala.main@gmail.com>
2024-02-13 18:11:22 +0200

    minor updates

--- src/components/DishOfTheDay.js
+++ src/components/DishOfTheDay.js
@@ -6,13 +6,15 @@ export default function (){
     const dish = data.special;
 
     const style = {
-        width :"400px",
+        width :"600px",
         height : "400px",
         backgroundImage: `url(${dish.image})`,
         backgroundSize: "cover", // resize the image to cover the div
         backgroundPosition: "center",
         boxShadow: "5px 5px 5px 3px black",
-        borderRadius : "1vh"
+        borderRadius : "1vh",
+        marginRight : "150px"
+        
 
     }
     return(
@@ -26,7 +28,6 @@ export default function (){
                 <div id="btn">
                     <input id="reservation-btn" value="Make Reservation" type="button" 
                     onClick={() => window.location.pathname = "/reservation"}/>
-
                 </div>
             </div>
 
