63147214a530f16a82de73d3f207e03258bc06d2
Bhekumuzi Tshabalala <Bhekumuzitshabalala.main@gmail.com>
2024-02-26 13:16:51 +0200

    footer finished ,home page finished , reservations page (ongoing)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,32 @@
+import emailIcon from "../assets/social-icons/email.svg"
+import instagramIcon from "../assets/social-icons/instagram.svg"
+import facebookIcon from "../assets/social-icons/facebook.svg"
+import twitterIcon from "../assets/social-icons/twitter.svg"
+import logo from "../assets/logo.png"
+
+import "./footer.css"
+
 export default function Footer(){
-    return (<div>
-        <h1>Footer</h1>
-    </div>)
+    return (
+        <footer className="footer">
+            <div >
+                <img src={logo} id="logo"/>
+                <p> Little Lemon, a delightful family-owned Mediterranean restaurant that transports diners to the sun-kissed shores of the Mediterranean</p>
+                <p>@bhekumuziTshabalala all rights reserved</p>
+            </div>
+            
+            <div className="socials">
+                <img src={emailIcon} alt="email" id="socialMedia" />
+                <img src={instagramIcon}  alt="instagram" id="socialMedia"/>
+                <img src={facebookIcon}  alt="facebook" id="socialMedia"/>
+                <img src={twitterIcon}  alt="twitter" id="socialMedia"/>
+            </div>
+
+            {/* <div>
+                
+                <h1>@bhekumuziTshabalala all rights reserved</h1>
+                <h2>This is a demo Site for a fictional resturant called little lemon</h2>
+            </div> */}
+        </footer>
+    )
 }
--- src/components/Reservation.js
+++ src/components/Reservation.js
@@ -1,3 +1,66 @@
-export default function Reservations(){
-    return (<h1>Reservations</h1>)
+import React, { useState } from 'react';
+import './reservation.css'
+
+function Reservation() {
+  const [name, setName] = useState('');
+  const [selectedOption, setSelectedOption] = useState('');
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  return (
+    <div>
+      <form className='form'>
+        
+        <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={handleNameChange}
+            placeholder="Full Names"
+            required
+        />
+
+        <br/>
+        <input
+            type="email"
+            name="email"
+            value={name}
+            onChange={handleNameChange}
+            placeholder="Email"
+            required
+        />
+
+        <br/>
+        <input
+            type="email"
+            name="email"
+            value={name}
+            onChange={handleNameChange}
+            placeholder="Email"
+            required
+        />
+        <br/>
+        <select
+          id="myDropdown"
+          value={selectedOption}
+          onChange={handleOptionChange}
+        >
+          <option value="">Choose a Restaurant</option>
+          <option value="option1">Option 1</option>
+          <option value="option2">Option 2</option>
+          <option value="option3">Option 3</option>
+        </select>
+        
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
+
+export default Reservation;
